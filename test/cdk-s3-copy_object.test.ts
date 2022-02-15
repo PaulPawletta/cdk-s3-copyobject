3c16ff1f3587399e7e8f6d32f72351ab5b827038
PaulPawletta <paul.pawletta@gmail.com>
2022-02-15 11:15:17 +0100

    Initial commit

---
+++ test/cdk-s3-copy_object.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkS3CopyObject from '../lib/cdk-s3-copy_object-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-s3-copy_object-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkS3CopyObject.CdkS3CopyObjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
